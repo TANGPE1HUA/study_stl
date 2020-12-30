522696f4defe3a67bbc989c97387b7f6fdbb0218
TANGPE1HUA <764776576@qq.com>
2020-12-30 17:03:02 +0800

    update code

---
+++ cmake-build-debug/CMakeFiles/study_stl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/study_stl.dir/allocator.cpp.o"
+  "study_stl"
+  "study_stl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/study_stl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
